07e4904f82144f4b426885fafd1757987d43cb8f
hftamayo <hftamayo@gmail.com>
2023-05-23 10:02:27 -0600

    alpha version of the project

--- app/src/main/java/com/hftamayo/kotlinuserscrud/UserList.java
+++ app/src/main/java/com/hftamayo/kotlinuserscrud/UserList.java
@@ -1,14 +1,48 @@
 package com.hftamayo.kotlinuserscrud;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.database.Cursor;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class UserList extends AppCompatActivity {
+    RecyclerView recyclerView;
+    ArrayList<String> name, email, age;
+    DBHelper db;
+    UsersListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_list);
+        db = new DBHelper(this);
+        name = new ArrayList<>();
+        email = new ArrayList<>();
+        age = new ArrayList<>();
+        recyclerView = findViewById(R.id.displayusers);
+        adapter = new UsersListAdapter(this, name, email, age);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        displayData();
+    }
+
+    private void displayData() {
+        Cursor cursor = db.getData();
+        if(cursor.getCount() == 0){
+            Toast.makeText(UserList.this, "No Entry Exists", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        else{
+            while(cursor.moveToNext()){
+                name.add(cursor.getString(0));
+                email.add(cursor.getString(1));
+                age.add(cursor.getString(2));
+            }
+        }
     }
 }
